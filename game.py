d7fe5407d540ea92c5de68ca2f528138981fccb1
asaf_ashwal <ashwalasafx@gmail.com>
2025-10-29 12:00:51 +0200

    last commit

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 from DB import todo_list
 
+
+
+
 def main() -> None:
     the_list = todo_list
     flag = True
@@ -7,24 +10,20 @@ def main() -> None:
         user_choice = get_user_choice()
         match user_choice:
             case '1':
-                new_task = input('enter new task: ')
+                new_task = get_inputs('enter new task: ')
                 add_task(the_list,new_task)
                 print('the list: ',the_list)
             case '2':
                 show_all_tasks(the_list)
             case '3':
-                if len(the_list) == 0 :
-                    print('list is empty')
-                    continue
-                index = input('enter task num to delete: ')
-                delete_task(the_list,index)
+                is_list(delete_task,the_list)
                 
             case '4':
-                if len(the_list) == 0 :
+                if not len(the_list) :
                     print('list is empty')
                     continue
-                index = input('enter task num to edit: ')
-                new_task = input('enter new task: ')
+                index = get_inputs('enter task num to edit: ')
+                new_task = get_inputs('enter new task: ')
                 edit_task(the_list,index, new_task)
             case '5':
                 print('exit')
@@ -33,6 +32,9 @@ def main() -> None:
                 print('an expected choice')
 
     
+
+
+
 
 
 def get_user_choice() -> str:
@@ -49,14 +51,23 @@ def get_user_choice() -> str:
 
 
 
+
+
+
 def add_task(tasks: list, task: str) -> None:
     tasks.append(task)
     
-    
+
+
+
+
 def show_all_tasks(tasks: list) -> None:
     if  len(tasks):
         print({'list':tasks})
     else: print('list is empty')
+
+
+
 
 
 def delete_task(tasks: list, index: int) -> bool:
@@ -64,13 +75,35 @@ def delete_task(tasks: list, index: int) -> bool:
     task = tasks.pop(index)
     print(f'you deleted {task}')
     return True
-        
+
+
+
+
 def edit_task(tasks: list, index: int, new_task: str) -> bool:
     index = int(index) -1
     if len(tasks) < index:
         print('en volid index')
         False
-        
     else: tasks[index] = new_task
-    
-    
+
+
+
+
+def get_inputs(text):
+    flag = True
+    while flag:
+        user_input = input(text)
+        if len(user_input) <= 0:
+            print("cenot send it empty ")
+            continue
+
+
+
+
+def is_list(func,my_list):
+    if len(my_list):
+        index = get_inputs('enter task num to delete: ')
+        func(my_list,index)
+    print('list is empty')
+                 
+                 
